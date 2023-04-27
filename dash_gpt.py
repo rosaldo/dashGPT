37f469b88090478616c0d94ca5de0b58d4beb8e3
Rosaldo <rosaldo@gmail.com>
2023-04-27 02:17:02 -0300

    Version optimized for handling the configuration file and partially commented.

--- dash_gpt.py
+++ dash_gpt.py
@@ -1,41 +1,88 @@
 #!/usr/bin/env python3
 # coding: utf-8
 
+# importa o módulo base64 para trabalhar com codificação e decodificação de dados
 import base64
+# importa o módulo os para interagir com o sistema operacional
 import os
 
+# importa o Dash Bootstrap Components para trabalhar com elementos visuais do Dash
 import dash_bootstrap_components as dbc
+# importa o módulo llama_index para indexação e busca em modelos de linguagem
 import llama_index
+# importa os componentes essenciais do Dash para criar a aplicação
 from dash import Dash, Input, Output, State, dcc, html
+# importa o Flask para criar o servidor web
 from flask import Flask
+# importa a classe ChatOpenAI do módulo chat_models do pacote langchain
 from langchain.chat_models import ChatOpenAI
-from llama_index import (GPTSimpleVectorIndex, LLMPredictor, ServiceContext,
-                         download_loader)
+# importa classes e função que são usadas para criar um indexador de dados para realizar buscas
+# por similaridade em textos e carregar um modelo de predição de linguagem natural
+from llama_index import GPTSimpleVectorIndex, LLMPredictor, ServiceContext, download_loader
 
+# verifica se o arquivo "config.py" já existe
+if not os.path.exists("config.py"):
+    # solicita que o usuário digite a chave de API do OpenAI e armazena na variável "api_key"
+    api_key = input("Digite a chave de API do OpenAI: ")
+    # abre (e se não existe, cria) o arquivo "config.py" no modo de escrita e armazena na variável "conf"
+    conf = open("config.py", "w")
+    # escreve a chave de API no arquivo "config.py"
+    conf.write(f"""OPENAI_API_KEY = "{api_key}"\n""")
+    # fecha o arquivo "config.py"
+    conf.close()
+
+# importa a chave de acesso da API do OpenAI da variável OPENAI_API_KEY do módulo config
 from config import OPENAI_API_KEY
 
+# define a variável de ambiente para a chave da API do OpenAI
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+# obter o caminho absoluto do diretório atual
 path = os.path.dirname(os.path.abspath(__file__))
+# define o caminho para o diretório onde os arquivos serão salvos
 folder = f"{path}/dash_gpt_pdf/"
+# define o nome do arquivo JSON que será utilizado para indexar os modelos de linguagem treinados
 index_json = "dash_gpt.json"
+# verifica se o diretório existe e, se não existir, cria o diretório
 if not os.path.exists(folder):
     os.makedirs(folder)
 
+# função de criação do índice de vetor simples a partir dos documentos PDF carregados
 def create_index():
+    # importação do loader "PDFReader" do pacote llama_index
     pdf_reader = download_loader("PDFReader")
+    # listagem dos arquivos do diretório definido
     files = os.listdir(folder)
+    # criação de uma lista vazia para armazenar os documentos
     documents = []
+    # laço que percorre os arquivos da listagem armazenada em "files"
     for file in files:
+        # verifica se o arquivo possui a extensão ".pdf"
         if file.endswith(".pdf"):
+            # armazena o caminho completo do arquivo
             pdf = f"{path}/dash_gpt_pdf/{file}"
+            # carregamento do documento PDF
             document = pdf_reader().load_data(pdf)
+            # adição do documento à lista de documentos
             documents.append(document[0])
-    llm_predictor = LLMPredictor(llm=ChatOpenAI(model_name="gpt-4-32k"))
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, chunk_size_limit=4096)
-    index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
-    index.save_to_disk(index_json)
+    # tenta executar o bloco abaixo
+    try:
+        # criação do objeto "LLMPredictor" com o modelo GPT-4-32K do OpenAI
+        llm_predictor = LLMPredictor(llm=ChatOpenAI(model_name="gpt-4-32k"))
+        # criação do objeto "ServiceContext" com as configurações padrão e com o objeto "LLMPredictor"
+        service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, chunk_size_limit=4096)
+        # criação do índice a partir dos documentos carregados, utilizando o objeto "GPTSimpleVectorIndex"
+        index = GPTSimpleVectorIndex.from_documents(documents, service_context=service_context)
+        # salvamento do índice em disco
+        index.save_to_disk(index_json)
+    # se houver uma exceção (erro) em qualquer parte do bloco
+    except:
+        # termina com um código de saída 1
+        os.sys.exit(1)
 
+# executa a função "create_index" definida acima
 create_index()
+# carrega o índice salvo anteriormente utilizando o método "load_from_disk"
+# da classe "GPTSimpleVectorIndex" e atribui o resultado à variável "index"
 index = GPTSimpleVectorIndex.load_from_disk(index_json)
 
 server = Flask(__name__)
