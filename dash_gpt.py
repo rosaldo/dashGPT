4df4b72dd0b234e7848cdd09670277b01e0dde46
Rosaldo <rosaldo@gmail.com>
2023-04-30 22:11:28 -0300

    v4.0.0 => implementation of using the ConfigParser library when working with the configuration file

--- dash_gpt.py
+++ dash_gpt.py
@@ -5,6 +5,8 @@
 import base64
 # importa o módulo os para interagir com o sistema operacional
 import os
+# importa o objeto ConfigParser do modulo configparser para gerenciar o arquivo de configuração
+from configparser import ConfigParser
 
 # importa o Dash Bootstrap Components para trabalhar com elementos visuais do Dash
 import dash_bootstrap_components as dbc
@@ -18,16 +20,19 @@ from flask import Flask
 from langchain.chat_models import ChatOpenAI
 # importa classes e função que são usadas para criar um indexador de dados para realizar buscas
 #   por similaridade em textos e carregar um modelo de predição de linguagem natural
-from llama_index import GPTSimpleVectorIndex, LLMPredictor, ServiceContext, download_loader
+from llama_index import (GPTSimpleVectorIndex, LLMPredictor, ServiceContext,
+                         download_loader)
 
 # registra a atual versão do app
-version = "3.0.1"
+version = "4.0.0"
 # obter o caminho absoluto do diretório atual
 path = os.path.dirname(os.path.abspath(__file__))
 # define o caminho para o diretório onde os arquivos serão salvos
 folder = f"{path}/dash_gpt_pdf/"
-# define o nome do arquivo .py que será utilizado para armazenar a chave de API do OpenAI
-config_py = "config.py"
+# criacao do objeto "config" para gerenciar o trabalho com o arquivo de configuração .ini
+config = ConfigParser()
+# define o nome do arquivo .ini que será utilizado para armazenar a chave de API do OpenAI
+config_ini = "dash_gpt.ini"
 # define o nome do arquivo JSON que será utilizado para indexar os modelos de linguagem treinados
 index_json = "dash_gpt.json"
 # verifica se o diretório existe e, se não existir, cria o diretório
@@ -157,10 +162,12 @@ def uppdf(files, contents):
 )
 def answer(enter, click, query, out):
     # verifica se o arquivo "config.py" já existe
-    if not os.path.exists(config_py):
+    if not os.path.exists(config_ini):
+        # se não possui qualquer histórico
         if not out:
             # solicita que o usuário digite a chave de API do OpenAI
             out.append(html.Dd("Não encontrei a chave da API do OpenAI. Por favor, digite a chave e pressione ENVIAR"))
+        # se possui alguma informação digitada pelo usuário
         if query:
             # define a variável de ambiente para a chave da API do OpenAI
             os.environ["OPENAI_API_KEY"] = query
@@ -168,12 +175,14 @@ def answer(enter, click, query, out):
             #   se a chave estevier ok, returna verdadeiro e salva no arquivo "config.py"
             #   caso contrário retorna falso e solicita novamente a chave
             if create_index(True):
-                # abre (e se não existe, cria) o arquivo "config.py" no modo de escrita e armazena na variável "conf"
-                conf = open(config_py, "w")
-                # escreve a chave de API no arquivo "config.py"
-                conf.write(f"""OPENAI_API_KEY = "{query}"\n""")
-                # fecha o arquivo "config.py"
-                conf.close()
+                # cria a seção "OPENAI"
+                config.add_section("OPENAI")
+                # cria na seção "OPENAI" o parâmetro "OPENAI_API_KEY" com o valor da variável "query"
+                config.set("OPENAI", "OPENAI_API_KEY", query)
+                # abre (e se não existe, cria) o arquivo "dash_gpt.ini" no modo de escrita
+                with open(config_ini, "w") as config_file:
+                    # escreve o conteúdo da variável "config" no arquivo "dash_gpt.ini"
+                    config.write(config_file)
                 # define o conteúdo da variável "query" para uma string vazia
                 query = ""
                 # define o conteúdo da variável "out" para uma lista vazia
@@ -183,18 +192,20 @@ def answer(enter, click, query, out):
             else:
                 # informa o usuário que a chave informada é inválida e solicita-a novamente
                 out.append(html.Dd("A chave da API do OpenAI informada é inválida. Por favor, digite uma chave válida e pressione ENVIAR"))
-    if os.path.exists(config_py) and not os.path.exists(index_json):
-        # importa a chave de acesso da API do OpenAI da variável OPENAI_API_KEY do módulo config
-        from config import OPENAI_API_KEY
+    # se existe o arquivo de configuração e não existe o arquivo de index
+    if os.path.exists(config_ini) and not os.path.exists(index_json):
+        # carrega a chave de acesso da API do OpenAI da variável OPENAI_API_KEY do arquivo de configuração
+        config.read(config_ini)
         # define a variável de ambiente para a chave da API do OpenAI
-        os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+        os.environ["OPENAI_API_KEY"] = config.get("OPENAI", "OPENAI_API_KEY")
         # executa a função que cria o índice
         create_index()
-    if query and os.path.exists(config_py) and os.path.exists(index_json):
-        # importa a chave de acesso da API do OpenAI da variável OPENAI_API_KEY do módulo config
-        from config import OPENAI_API_KEY
+    # se existe alguma mensagem do usuário e existem os arquivos de configuração e de index
+    if query and os.path.exists(config_ini) and os.path.exists(index_json):
+        # carrega a chave de acesso da API do OpenAI da variável OPENAI_API_KEY do arquivo de configuração
+        config.read(config_ini)
         # define a variável de ambiente para a chave da API do OpenAI
-        os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
+        os.environ["OPENAI_API_KEY"] = config.get("OPENAI", "OPENAI_API_KEY")
         # carrega o índice salvo anteriormente utilizando o método "load_from_disk"
         #   da classe "GPTSimpleVectorIndex" e atribui o resultado à variável "index"
         index = GPTSimpleVectorIndex.load_from_disk(index_json)
@@ -204,6 +215,7 @@ def answer(enter, click, query, out):
         out.append(html.Dt(query))
         # adiciona a resposta dada ao conteúdo da variável "out"
         out.append(html.Dd(response))
+    # retorna o conteúdo dos widgets dash
     return [out, "", out]
 
 self_name = os.path.basename(__file__)[:-3]
